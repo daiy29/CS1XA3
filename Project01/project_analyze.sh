f707107b4b4db5d518b0d598d7b7eb6c9769f9d9
daiy29 <daiy29@mac1xa3.ca>
2019-02-27 03:18:05 +0000

    Added comments

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -7,18 +7,23 @@ echo "3) ""Count File Types"
 echo "4) ""Create GitHub Directory"
 echo "5) ""Have some fun!"
 echo "6) ""Backup Directory as Zip File & Upload to GitHub"
-read task
-if [ $task == "1" ] ; then
+
+#Interface for users to select the task that would like to be executed
+#A new line is given for every choice so it is asthetically pleasing
+
+
+read task #Read user choice
+if [ $task == "1" ] ; then #Choice 1 executes deleting temperary files
  	echo "Deleting Temporary Files"
-	tempfiles=$(git ls-files . --exclude-standard --others)
-	find $tempfiles -type f -name '*.tmp' | xargs rm 2>/dev/null
+	tempfiles=$(git ls-files . --exclude-standard --others) #All untracked files listed in current directory assigned to var  tempfiles
+	find $tempfiles -type f -name '*.tmp' | xargs rm 2>/dev/null #Find these files with extension .tmp, remove them 2>/dev/null throws away error codes from no file found
 
-elif [ $task == "2" ] ; then
+elif [ $task == "2" ] ; then #Choice 2 executes moving TODO to file log
 	echo "Moving TODOS to file todo.log"
-	(grep -r -h  "#TODO" --exclude="project_analyze.sh" --exclude="todo.log") > todo.log
+	(grep -r -h  "#TODO" --exclude="project_analyze.sh" --exclude="todo.log") > todo.log #find #TODO in files, excludes this file as well as the todo.log itself
 
-elif [ $task == "3" ] ; then
-        html=`find . -type f -name '*.html' | wc -l`
+elif [ $task == "3" ] ; then #Choice 3 counts each file type and spits out number
+        html=`find . -type f -name '*.html' | wc -l` #Find files, ending in extension corresponding to file type, assigns it to variable , count lines
         javascript=`find . -type f -name '*.js' | wc -l`
 	css=`find . -type f -name '*.css' | wc -l`
 	python=`find . -type f -name '*.py' | wc -l`
@@ -31,38 +36,38 @@ elif [ $task == "3" ] ; then
 	echo "Python:" $python
 	echo "Haskell:" $haskell
 	echo "Bash:" $bash
-elif [ $task = "4" ] ; then
-        echo "What is the name of your directory?"
+elif [ $task = "4" ] ; then #Choice 4 automatically creates a github directory
+        echo "What is the name of your directory?" #Read user input, and options
 	read directory
 	echo "What is your commit message?"
 	read message
-	mkdir $directory
+	mkdir $directory #Creates a directory with a readme, and echos TODO in the readme file.
 	touch $directory/README.md
 	echo "#TODO" >> $directory/README.md
 	git add $directory
 	git commit -m "$message"
-	git push -f origin master
+	git push -f origin master  #Adds, commits, and pushes the new directory
 	echo "The directory has been created, added, commited, and pushed."
-elif [ $task = "5" ] ; then
+elif [ $task = "5" ] ; then 
 	echo "Playing Star Wars in 3!!!"
 	sleep 1s
 	echo "2!!!"
 	sleep 1s
 	echo "1!!!"
 	sleep 1s
-	telnet towel.blinkenlights.nl
-elif [ $task = "6" ] ; then
+	telnet towel.blinkenlights.nl #Sorta a meme feature :P Just plays starwars
+elif [ $task = "6" ] ; then # Task 6 backs up files and uploads them directory to your github.
 	echo "What will the name of your cloned directory be?"
-	read destn
+	read destn 
 	source=~/CS1XA3/Project01
         destination=~/CS1XA3/$destn
-        filename=ug-$(date +%-Y%-m%-d)-$(date +%-T).tgz
+        filename=ug-$(date +%-Y%-m%-d)-$(date +%-T).tgz #File name created with name of cloning
         tar --create --gzip --file=$destination$filename $source
 	echo "It has been cloned"
 	git add -A
 	git commit -m "Backup up files"
 	git push -f origin master
-	echo "The zip files has been uploaded to GitHub"
+	echo "The zip files has been uploaded to GitHub" #Added, commited, pushed.
 # elif [ $task = "6" ] ; then
 #	echo "Generating Error Log"
 #	find . -name "*.hs" -exec ghc -fno-code {} \; &> compile_fail.log
