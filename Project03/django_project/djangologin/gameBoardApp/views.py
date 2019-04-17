127f4edcd13d82acac6e376e70e4de4e961fa6e0
daiy29 <daiy29@mac1xa3.ca>
2019-04-17 18:24:25 +0000

    Test for recent scoreboard, will likely change login app next commit because it doesnt use JSON requests

---
+++ Project03/django_project/djangologin/gameBoardApp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+
+def createpost(request):
+        if request.method == 'POST':
+            if request.POST.get('title') and request.POST.get('content'):
+                post=Post()
+                post.title= request.POST.get('title')
+                post.content= request.POST.get('content')
+                post.save()
+                
+                return render(request, 'posts/create.html')  
+
+        else:
+                return render(request,'posts/create.html')
+
+def showboard(request):
+ 
+    allscores= Post.objects.all()
+    
+    context= {'allscores': allscores}
+
+        
+    return render(request, 'gameBoardApp/scoreboard.html', context)
